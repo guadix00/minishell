5a4883cfd804061dc13441555302f3f9a8a38088
guadix00 <guadit64@gmail.com>
2025-01-18 20:03:17 +0100

    no entiendo que esta pasando

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,10 +6,16 @@ void parent_signals(void)
     signal(SIGQUIT, SIG_IGN);
 }
 
+/*void child_signals(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}*/
+
 void child_signals(void)
 {
-    signal(SIGINT, SIG_DFL);
-    signal(SIGQUIT, SIG_DFL);
+	signal(SIGINT, display_new_line);
+	signal(SIGQUIT, display_new_line);
 }
 
 void here_signals(void)
@@ -29,7 +35,7 @@ void output_signals(int sig)
 void display_new_line(int sig)
 {
     (void)sig;
-    printf("\n");
+    printf("hijos\n");
     rl_on_new_line();
     rl_redisplay();
 }
